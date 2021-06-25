fa91102df23c05aeb06d7943b6d9c294cb963f23
Roberto Vázquez Iglesias <meleno@gmail.com>
2021-06-25 19:50:35 +0200

    added new classes

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace Ordering.Application.Mappings
+{
+	class MappingProfile : Profile
+	{
+        public MappingProfile()
+        {
+            //CreateMap<Order, OrdersVm>().ReverseMap();
+            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
