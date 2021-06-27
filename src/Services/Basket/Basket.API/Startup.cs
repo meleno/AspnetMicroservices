4a5145430d7a9d94c9f3d493cd92e376c0a0ad56
Roberto Vázquez Iglesias <meleno@gmail.com>
2021-06-27 12:06:28 +0200

    Added masstransit to ioc at startup

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,18 +1,14 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Discount.Grpc.Protos;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.API
 {
@@ -36,6 +32,16 @@ namespace Basket.API
 			services.AddScoped<IBasketRepository, BasketRepository>();
 			services.AddScoped<DiscountGrpcService>();
 			services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(x => x.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+
+			services.AddMassTransit(config =>
+			{
+				config.UsingRabbitMq((ctx, cfg) =>
+				{
+					cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+				});
+			});
+
+			services.AddMassTransitHostedService();
 
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
